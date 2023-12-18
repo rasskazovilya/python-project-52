72e2f9ff342e21bc4ab5c1959a74ece677a7ae54
Ilya <rasskazov_ilya_98@mail.ru>
2023-12-18 15:01:24 +0300

    update settings to run on render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,11 +26,15 @@ dotenv.load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = [
     "webserver",
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition

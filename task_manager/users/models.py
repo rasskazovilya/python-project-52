c322f1ceb959339ea163e556ac46e5777a16c4d4
Ilya <rasskazov_ilya_98@mail.ru>
2023-12-21 11:16:25 +0300

    add simple User model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)

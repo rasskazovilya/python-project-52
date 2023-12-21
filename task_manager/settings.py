69bfe8372d202ad06e02ea79908be768f7999d7f
Ilya <rasskazov_ilya_98@mail.ru>
2023-12-21 10:41:04 +0300

    add django db config from url env variable

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -142,10 +142,15 @@ BOOTSTRAP5 = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 

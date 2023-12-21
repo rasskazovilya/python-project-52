b88306e25f12a5884d818e6754bbc313a50e3ebb
Ilya <rasskazov_ilya_98@mail.ru>
2023-12-21 11:18:37 +0300

    add simple UsersView without any functionality

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.views import View
+
+from task_manager.users.models import User
+
+
+class UsersView(View):
+    template_name = "users.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["users"] = User.objects.all()
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)

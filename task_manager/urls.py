659390c175ce8ec22dee4c39e656a47bad01a415
Ilya <rasskazov_ilya_98@mail.ru>
2023-12-22 15:21:59 +0300

    add urls for login and logout

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,6 +20,8 @@ from task_manager import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index),
+    path("", views.index, name="home"),
     path("users/", include("task_manager.users.urls")),
+    path("login/", views.UserLoginView.as_view(), name="login"),
+    path("logout/", views.UserLogoutView.as_view(), name="logout"),
 ]

03c4f9a54c426ba7201a397aaca58c33d8ea7ac6
Ilya <rasskazov_ilya_98@mail.ru>
2023-12-22 10:02:01 +0300

    fix urls

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,7 +4,7 @@ from task_manager.users import views
 
 urlpatterns = [
     path("", views.UsersView.as_view()),
-    path("create", views.UserCreateView.as_view()),
-    path("<int:id>/update", views.UserUpdateView.as_view()),
-    path("<int:id>/delete", views.UserDeleteView.as_view()),
+    path("create/", views.UserCreateView.as_view()),
+    path("<int:id>/update/", views.UserUpdateView.as_view()),
+    path("<int:id>/delete/", views.UserDeleteView.as_view()),
 ]

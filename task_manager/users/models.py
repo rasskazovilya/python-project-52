fcb51be3b44bae6da26b390eb0c2958f9de3c7d1
Ilya <rasskazov_ilya_98@mail.ru>
2023-12-22 10:17:58 +0300

    change username max length to 150

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=150)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     password = models.CharField(max_length=200)

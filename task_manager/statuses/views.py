7aa99c12fedcf7708e43fe2452b80afc3eef56b8
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-15 16:32:21 +0300

    add imports for status views

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.utils.translation import gettext
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+
 from .models import Status
+
 
 # Create your views here.
 class StatusListView(ListView):

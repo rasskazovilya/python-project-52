293c10a37be397c31a2381078d7bab5810935ef5
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-15 14:59:12 +0300

    create CRUD outline for status views

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
 # Create your views here.
+class StatusListView(ListView):
+    pass
+
+
+class StatusCreateView(CreateView):
+    pass
+
+
+class StatusUpdateView(UpdateView):
+    pass
+
+
+class StatusDeleteView(DeleteView):
+    pass

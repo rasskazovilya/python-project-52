14006652fe305fc8b8112298a38401644afad7dc
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-15 15:30:02 +0300

    add simple test case for status model

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Status
+from django.urls import reverse_lazy
 
 # Create your tests here.
+class StatusTestCase(TestCase):
+    fixtures = ["statuses.json"]
+
+    def setUp(self):
+        self.test_status = {
+            "name": "Done",
+        }
+
+        self.test_statuses = []
+
+        for i in range(5):
+            status = {
+                "name": f"Test_{i}",
+            }
+            self.test_statuses.append(status)
+        return super().setUp()
+
+    def test_status_list(self):
+        response = self.client.get(reverse_lazy("status_list"))
+
+        for status in self.test_statuses:
+            self.assertContains(response, status["name"])

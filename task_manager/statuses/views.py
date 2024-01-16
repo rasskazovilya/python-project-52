2c86ccb0cf0300bfb30649212102b4f3f66086b7
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-16 14:05:40 +0300

    add status update view

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
@@ -46,8 +47,24 @@ class StatusCreateView(LoginRequiredMixin, CreateView):
         return super().handle_no_permission()
 
 
-class StatusUpdateView(UpdateView):
-    pass
+class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    template_name = "status_create.html"
+    login_url = reverse_lazy("login")
+    success_url = reverse_lazy("status_list")
+    success_message = gettext("Статус успешно изменен.")
+    model = Status
+    extra_context = {"title": gettext("Изменить статус")}
+    fields = ["name"]
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                gettext("Вы не авторизованы! Пожалуйста, выполните вход."),
+                extra_tags="danger",
+            )
+            return redirect(self.login_url)
+        return super().handle_no_permission()
 
 
 class StatusDeleteView(DeleteView):

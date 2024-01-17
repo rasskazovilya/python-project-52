07485a235b1704b0f4cda08f22b161b086c08960
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-17 14:33:46 +0300

    change user views to use LoginRequiredMsgMixin

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 
 from django.shortcuts import redirect
@@ -8,6 +7,7 @@ from django.utils.translation import gettext
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from task_manager.users.forms import UserCreateForm
 from task_manager.users.models import User
+from task_manager.mixins import LoginRequiredMsgMixin
 
 
 class UserListView(ListView):
@@ -26,59 +26,43 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     extra_context = {"title": gettext("Создание пользователя")}
 
 
-class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+class UserUpdateView(LoginRequiredMsgMixin, SuccessMessageMixin, UpdateView):
     template_name = "user_create.html"
-    login_url = reverse_lazy("login")
     model = User
     success_url = reverse_lazy("user_list")
     success_message = gettext("Пользователь успешно изменен.")
     form_class = UserCreateForm
     extra_context = {"title": gettext("Изменение пользователя")}
 
-    def dispatch(self, *args, **kwargs):
-        if not self.request.user.is_authenticated:
-            messages.error(
-                self.request,
-                gettext("Вы не авторизованы! Пожалуйста, выполните вход."),
-                extra_tags="danger",
-            )
-            return redirect(self.login_url)
+    def get(self, request, *args, **kwargs):
         same_user = self.model.objects.get(pk=kwargs["pk"])
-        if self.request.user != same_user:
+        if request.user != same_user:
             messages.error(
                 self.request,
                 gettext("У вас нет прав для изменения другого пользователя."),
                 extra_tags="danger",
             )
             return redirect(self.success_url)
-        return super().dispatch(*args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
 
-class UserDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+class UserDeleteView(LoginRequiredMsgMixin, SuccessMessageMixin, DeleteView):
     template_name = "confirm_delete.html"
-    login_url = reverse_lazy("login")
     model = User
     success_url = reverse_lazy("user_list")
     success_message = gettext("Пользователь успешно удален.")
     extra_context = {"title": gettext("Удаление пользователя")}
 
-    def dispatch(self, *args, **kwargs):
-        if not self.request.user.is_authenticated:
-            messages.error(
-                self.request,
-                gettext("Вы не авторизованы! Пожалуйста, выполните вход."),
-                extra_tags="danger",
-            )
-            return redirect(self.login_url)
+    def get(self, request, *args, **kwargs):
         same_user = self.model.objects.get(pk=kwargs["pk"])
-        if self.request.user != same_user:
+        if request.user != same_user:
             messages.error(
                 self.request,
                 gettext("У вас нет прав для удаления другого пользователя."),
                 extra_tags="danger",
             )
             return redirect(self.success_url)
-        return super().dispatch(*args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
     def delete(self, *args, **kwargs):
         response = super().delete(*args, **kwargs)

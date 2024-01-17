af23aad4bd2924d1047f93c6999003523fb99533
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-17 15:49:28 +0300

    changed user views according to SameUserCheckMixin

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -7,7 +7,8 @@ from django.utils.translation import gettext
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from task_manager.users.forms import UserCreateForm
 from task_manager.users.models import User
-from task_manager.mixins import LoginRequiredMsgMixin
+from task_manager.mixins import LoginRequiredMsgMixin, SameUserCheckMixin
+from django.core.exceptions import PermissionDenied
 
 
 class UserListView(ListView):
@@ -26,43 +27,37 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     extra_context = {"title": gettext("Создание пользователя")}
 
 
-class UserUpdateView(LoginRequiredMsgMixin, SuccessMessageMixin, UpdateView):
+class UserUpdateView(
+    LoginRequiredMsgMixin, SuccessMessageMixin, SameUserCheckMixin, UpdateView
+):
     template_name = "user_create.html"
     model = User
     success_url = reverse_lazy("user_list")
     success_message = gettext("Пользователь успешно изменен.")
     form_class = UserCreateForm
     extra_context = {"title": gettext("Изменение пользователя")}
+    same_user_error_message = (
+        "У вас нет прав для изменения другого пользователя."
+    )
 
-    def get(self, request, *args, **kwargs):
-        same_user = self.model.objects.get(pk=kwargs["pk"])
-        if request.user != same_user:
-            messages.error(
-                self.request,
-                gettext("У вас нет прав для изменения другого пользователя."),
-                extra_tags="danger",
-            )
-            return redirect(self.success_url)
-        return super().get(request, *args, **kwargs)
+    def get_same_user(self, pk):
+        return self.model.objects.get(pk=pk)
 
 
-class UserDeleteView(LoginRequiredMsgMixin, SuccessMessageMixin, DeleteView):
+class UserDeleteView(
+    LoginRequiredMsgMixin, SuccessMessageMixin, SameUserCheckMixin, DeleteView
+):
     template_name = "confirm_delete.html"
     model = User
     success_url = reverse_lazy("user_list")
     success_message = gettext("Пользователь успешно удален.")
     extra_context = {"title": gettext("Удаление пользователя")}
+    same_user_error_message = (
+        "У вас нет прав для удаления другого пользователя."
+    )
 
-    def get(self, request, *args, **kwargs):
-        same_user = self.model.objects.get(pk=kwargs["pk"])
-        if request.user != same_user:
-            messages.error(
-                self.request,
-                gettext("У вас нет прав для удаления другого пользователя."),
-                extra_tags="danger",
-            )
-            return redirect(self.success_url)
-        return super().get(request, *args, **kwargs)
+    def get_same_user(self, pk):
+        return self.model.objects.get(pk=pk)
 
     def delete(self, *args, **kwargs):
         response = super().delete(*args, **kwargs)

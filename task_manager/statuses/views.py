de10f458428141a08f9e0e2f4de03c2513343407
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-17 11:37:21 +0300

    sort imports in statuses views.py

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect, render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView

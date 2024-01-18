0b32fdc229a6fdafeb29ffe2f16f1cac8e594e8d
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-18 14:42:11 +0300

    add first tasks test

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from task_manager.statuses.models import Status
+from task_manager.users.models import User
+from task_manager.tasks.models import Task
+from django.urls import reverse_lazy
 
 # Create your tests here.
+class TaskTestCase(TestCase):
+    fixtures = ["tasks.json"]
+
+    def setUp(self):
+        self.test_user = {
+            "username": "Test",
+            "first_name": "Test",
+            "last_name": "Test",
+            "password": "Test",
+        }
+        User.objects.create(**self.test_user)
+
+        # self.test_status = {
+        #     "name": "Done",
+        # }
+        # Status.objects.create(**self.test_status)
+
+        self.task = {
+            "name": "Task1",
+            "description": "Desc1",
+            "status": 1,
+            "creator": 1,
+            "performer": 1,
+        }
+
+        return super().setUp()
+
+    def test_status_list(self):
+        response = self.client.get(reverse_lazy("task_list"))
+        self.assertEqual(302, response.status_code)
+        self.assertRedirects(response, reverse_lazy("login"))
+
+        task = Task.objects.first()
+        self.client.force_login(self.test_user)
+        response = self.client.get(reverse_lazy("status_list"))
+
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, task.name)
+

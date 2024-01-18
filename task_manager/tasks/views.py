a7e9ff1b42f9f8f628f71084c7b9507db6f814be
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-18 14:41:17 +0300

    add view structure for tasks app

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    ListView,
+    UpdateView,
+    DetailView,
+)
+from task_manager.mixins import LoginRequiredMsgMixin
+
+from .models import Task
+
 
 # Create your views here.
+class TaskListView(LoginRequiredMsgMixin, ListView):
+    pass
+
+
+class TaskCreateView(LoginRequiredMsgMixin, CreateView):
+    pass
+
+
+class TaskUpdateView(LoginRequiredMsgMixin, SuccessMessageMixin, UpdateView):
+    pass
+
+
+class TaskDeleteView(LoginRequiredMsgMixin, SuccessMessageMixin, DeleteView):
+    pass
+
+
+class TaskDetailView(LoginRequiredMsgMixin, DetailView):
+    pass

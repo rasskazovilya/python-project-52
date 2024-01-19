7e7078effd7ea36367b3a630c3a8fa958436fa9f
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-19 12:17:15 +0300

    add view structure for labels

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    ListView,
+    UpdateView,
+    DetailView,
+)
+from task_manager.mixins import LoginRequiredMsgMixin
+
+from .models import Label
+
 
 # Create your views here.
+class LabelListView(LoginRequiredMsgMixin, ListView):
+    pass
+
+
+class LabelCreateView(LoginRequiredMsgMixin, CreateView):
+    pass
+
+
+class LabelUpdateView(LoginRequiredMsgMixin, SuccessMessageMixin, UpdateView):
+    pass
+
+
+class LabelDeleteView(LoginRequiredMsgMixin, SuccessMessageMixin, DeleteView):
+    pass
+
+
+class LabelDetailView(LoginRequiredMsgMixin, DetailView):
+    pass

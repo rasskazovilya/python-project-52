a8bc32b709996654ea836997427543c53b1f2523
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-19 09:33:24 +0300

    add tests for task editing

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -63,8 +63,36 @@ class TaskTestCase(TestCase):
         self.assertEqual(302, response.status_code)
         self.assertRedirects(response, reverse_lazy("task_list"))
 
-        # check if new status is added
+        # check if new task is added
         new_item = Task.objects.last()
         self.assertEqual(new_item.name, self.test_task["name"])
         self.assertEqual(new_item.creator, User.objects.get(id=1))
+
+    def test_edit_status(self):
+        edit_url = reverse_lazy("edit_task", kwargs={"pk": 1})
+
+        # check if unathorized user can not edit tasks
+        response = self.client.post(
+            edit_url, data=self.test_task, format="json"
+        )
+        self.assertEqual(302, response.status_code)
+        self.assertRedirects(response, reverse_lazy("login"))
+
+        # login user
+        user = User.objects.first()
+        self.client.force_login(user)
+        # change task
+        response = self.client.post(
+            edit_url, data=self.test_task, format="json", follow=True
+        )
+        # check if redirect is correct and success message is showing
+        self.assertEqual(200, response.status_code)
+        self.assertRedirects(response, reverse_lazy("task_list"))
+        self.assertContains(response, "Задача успешно изменена.")
+
+        # check if task has changed
+        edited_task = Task.objects.get(id=1)
+        self.assertEqual(edited_task.name, "Task1")
+        self.assertEqual(edited_task.description, "Desc1")
+        self.assertEqual(edited_task.creator, User.objects.first())
 

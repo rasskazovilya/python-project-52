ba0e550e8d2d8325e6c6b219caca634e4521c00a
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-19 12:17:30 +0300

    add label model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext
 
 # Create your models here.
+class Label(models.Model):
+    name = models.CharField(
+        max_length=255, unique=True, verbose_name=gettext("Имя")
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name=gettext("Дата создания")
+    )
+
+    def __str__(self):
+        return self.name

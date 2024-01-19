8dc912e1d6b13f719e2e436d04828c2ca05247b8
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-19 14:34:05 +0300

    add verbose name to status

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext
 
 # Create your models here.
 class Status(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(
+        max_length=255, unique=True, verbose_name=gettext("Имя")
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

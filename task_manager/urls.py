2e328597a8392b8bbcf29a256d29689c87b0d917
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-19 11:58:19 +0300

    update project urlconf with labels path

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("users/", include("task_manager.users.urls")),
     path("statuses/", include("task_manager.statuses.urls")),
     path("tasks/", include("task_manager.tasks.urls")),
+    path("labels/", include("task_manager.labels.urls")),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
 ]

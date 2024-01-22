88a31e356029b3f269ae969e0e8193f9a9bc06b7
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-22 14:47:50 +0300

    add django_filters as dependency

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "django_filters",
     "task_manager",
     "task_manager.users",
     "task_manager.statuses",

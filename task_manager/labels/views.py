5caf5191925a5936fe22af7323779b9dc2ed2191
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-22 12:08:07 +0300

    add label delete view

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -49,4 +49,24 @@ class LabelUpdateView(LoginRequiredMsgMixin, SuccessMessageMixin, UpdateView):
 
 
 class LabelDeleteView(LoginRequiredMsgMixin, SuccessMessageMixin, DeleteView):
-    pass
+    template_name = "confirm_delete.html"
+    success_url = reverse_lazy("label_list")
+    success_message = gettext("Метка успешно удалена.")
+    model = Label
+    extra_context = {"title": gettext("Удалить метку")}
+
+    def delete(self, *args, **kwargs):
+        del_label = self.model.objects.get(pk=kwargs["pk"])
+
+        if del_label.tasks.exists():
+            messages.error(
+                self.request,
+                gettext(
+                    "Невозможно удалить метку, потому что она используется"
+                ),
+                extra_tags="danger",
+            )
+            return redirect(self.success_url)
+        response = super().delete(*args, **kwargs)
+        messages.success(self.request, self.success_message)
+        return response

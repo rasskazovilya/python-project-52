0c9b36fa0bad3e7ff398277a66e8666e1647666a
Ilya <rasskazov_ilya_98@mail.ru>
2024-01-25 15:40:57 +0300

    add verbose names to user model fields

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -7,8 +7,14 @@ class User(AbstractUser):
     username = models.CharField(
         max_length=150, unique=True, verbose_name=gettext("Username")
     )
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+    first_name = models.CharField(
+        max_length=200, verbose_name=gettext("First Name")
+    )
+    last_name = models.CharField(
+        max_length=200, verbose_name=gettext("Last Name")
+    )
+    password = models.CharField(
+        max_length=200, verbose_name=gettext("Password")
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     USERNAME_FIELD = "username"
